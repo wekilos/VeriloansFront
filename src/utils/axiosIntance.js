c6f9e0e44bfe72a7d7ec93072b3ff650cb2ce69c
wekilos <wekilyazgulyyew2000@gmail.com>
2023-03-07 03:58:27 +0500

    add products

--- src/utils/axiosIntance.js
+++ src/utils/axiosIntance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { token } from "./token";
-const BASE_URL = "http://localhost:8181";
-// const BASE_URL = "http://119.235.116.165:8181";
+// const BASE_URL = "http://localhost:8181";
+const BASE_URL = "http://119.235.116.165:8181";
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
